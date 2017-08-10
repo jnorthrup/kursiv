71de72e84f0cbf5efd49f781870af5ac4b8856c7
Jim Northrup <jnorthrup@users.noreply.github.com>
2017-08-11 04:40:08 +0700

    some ideas on parser formulation

--- src/main/kotlin/std/std.kt
+++ src/main/kotlin/std/std.kt
@@ -2,16 +2,8 @@ package std;
 
 fun <T> cat(vararg b: Sequence<T>) = b.reduce { a, x-> a+x}
 
-
-
-
-
-
 fun bstr(k: Sequence<Byte>) =String(k.toList().toByteArray(), Charsets.UTF_8)
-
-
 fun cstr(k: Sequence<Char>) = String(k.toList().toCharArray())
-
 fun str(vararg k:  Byte ) =String(k)
-
 fun str(vararg k: Char  ) = String(k)
+
